0ec231305e4c298b8c458b34eb0b38621af10466
Adrian Barros <adriansbarros1@gmail.com>
2023-09-09 14:37:22 -0400

    Porject start && React Typescript installed && folder structure added

---
+++ dog-food-calc/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Home } from './Pages/home';
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ dog-food-calc/src/Pages/home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+interface Props {
+
+}
+
+export const Home = () => {
+
+    return (
+        <div>
+            Hello food calc
+        </div>
+    )
+}
+
